1ad10f04a9d54bcfd634aaa89754d22664e1ecb8
Steven Diaz <stevenbdf@gmail.com>
2021-04-07 23:53:06 -0600

    🎉 First commit

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+client.on('ready', () => {
+  console.log(`Logged in...`);
+});
+
+function isValidURL(string) {
+  var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  return (res !== null)
+};
+
+client.on('message', msg => {
+  const CHANNEL_ID = process.env.CHANNEL_ID;
+
+  if (msg.channel.id === CHANNEL_ID) {
+    if (!isValidURL(msg.content) && !msg.author.bot) {
+      msg.delete()
+        .then(async () => {
+          msg.channel.send('Mensaje borrado:\n**Solo se permiten links en este channel. No seas puto.**').then((msg) => {
+            setTimeout(() => {
+              msg.delete();
+            }, 1000)
+          })
+        })
+        .catch((err) => console.log(err))
+    }
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
